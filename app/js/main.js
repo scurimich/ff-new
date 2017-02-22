ed7218c36af1b4a814b86da0d346896fb0d8ca28
Vladislav Tsykota <tsykota.vlad@gmail.com>
2017-02-22 17:43:43 +0300

    fixed select, second buttons in atoms

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  $('.select').selectric();
+  $('.select').selectric({
+    onChange: selectricDarken,
+    onInit: selectricDarken
+  });
 });
+
+function selectricDarken(select) {
+  var $select = $(select);
+  var disabled = $select.find('option[data-disabled]').text();
+  var selectric = $select.parents('.selectric-select');
+  var label = selectric.find('.selectric .label');
+  label.removeClass('disabled');
+  console.log(label.text(), disabled);
+  if(label.text() === disabled) label.addClass('disabled');
+}
