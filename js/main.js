a2fc2f910c9413063bdf817a09d26f620313b92c
scurimich <tsykota.vlad@gmail.com>
2017-08-03 16:10:02 +0300

    Update 2017-08-03T13:09:47.425Z

--- js/main.js
+++ js/main.js
@@ -658,6 +658,7 @@ function googleMapInit() {
 
     $options.map(function(ndx, option) {
     	var option = $(option);
+      if (option.attr('data-disabled') !== undefined) return;
     	var selected = option.prop('selected') ? 'selected' : '';
     	var disabled = option.prop('disabled') ? ' disabled' : '';
     	var value = option.attr('value');
