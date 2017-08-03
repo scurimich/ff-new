3fdd3184131ca69cb10755a58859c5d59864ffc1
scurimich <tsykota.vlad@gmail.com>
2017-08-03 15:02:01 +0300

    Update 2017-08-03T12:01:37.079Z

--- js/main.js
+++ js/main.js
@@ -71,8 +71,6 @@ $(function() {
     listeners: function() {
       $('.expand-text').click(expandText);
 
-      $('.select-box_multiple').on('click', '.cancel', this.clearMultiselect);
-
       $(document).click(this.popup);
 
       $('[id=commenting-input]').focus(this.npWrite);
@@ -114,14 +112,13 @@ $(function() {
     },
 
     plugins: function() {
-      $('.select').selectric({
+      $('.select:not([multiple])').selectric({
         onChange: this.selectricOnChange,
         onInit: this.selectricOnInit,
-        multiple: {
-          separator: ', ',
-          keepMenuOpen: true,
-          maxLabelEntries: false
-        }
+      });
+
+      $('.select[multiple]').multiselect({
+        maxSize: 6
       });
 
       $('.rating').rateit({
@@ -185,7 +182,14 @@ $(function() {
     },
 
     datepickerShow: function() {
-      $(this).next().datepicker('show');
+      var $this = $(this);
+      if($this.is('.showed')) {
+        $this.next().datepicker('hide');
+        $this.removeClass('showed');
+      } else {
+        $this.next().datepicker('show');
+        $this.addClass('showed');
+      }
     },
 
     timeValidation: function() {
@@ -220,25 +224,7 @@ $(function() {
       var $select = $(select);
       var disabled = $select.find('option[data-disabled]').text();
       var selectric = $select.parents('.selectric-select');
-      if ($select.parents('.owner-main__select')) {
-        var options = $select.find('option');
-        var items = selectric.find('.selectric-items .selected');
-        if (items.length >= 3) {
-          selectric.find('.selectric-items li:not(.selected)').each(function() {
-            var li = $(this);
-            var index = li.index();
-            li.addClass('disabled');
-            $select
-              .find('option:nth-child('+ (index + 1) +')')
-              .prop('disabled', true);
-          });
 
-        } else if (items.length == 2 && $select.find('option:disabled').length){
-          // selectric.find('.selectric-items li')
-          // options.attr('disabled', false);
-          // $select.selectric('refresh').click();
-        }
-      }
       var labelParent = selectric.find('.selectric');
       var label = selectric.find('.selectric .label');
       label.removeClass('disabled');
@@ -254,15 +240,17 @@ $(function() {
       }
     },
 
-    clearMultiselect: function(e) {
-      e.preventDefault();
-      var button = $(this);
-      var select = button.parents('.selectric-select').find('.selectric-items li.selected');
-      select.each(function() {
-        $(this).click();
-      });
-      $(this).remove();
-    },
+    // clearMultiselect: function(e) {
+    //   e.preventDefault();
+    //   var button = $(this);
+    //   var parent = button.parents('.selectric-wrapper');
+    //   var select = button.parents('.selectric-select').find('.selectric-items li.selected');
+    //   select.each(function() {
+    //     $(this).removeClass('selected');
+    //   });
+    //   parent.find('select option').each(function() {$(this).prop('selected', false)})
+    //   $(this).remove();
+    // },
 
     popup: function(e) {
       var click = $(e.target);
@@ -649,3 +637,130 @@ function googleMapInit() {
     marker.setPosition(coords);
   });
 }
+(function($) {
+  $.fn.multiselect = function(settings) {
+
+  	var $this = $(this);
+
+  	var $options = $this.find('option');
+  	var firstLabelText = $($options[0]).val();
+
+    var wrapper = $('<div/>', {'class': 'selectric-wrapper selectric-select'});
+    var selectric = $('<div/>', {'class': 'selectric'});
+    var items = $('<div/>', {'class': 'selectric-items'});
+    var hide = $('<div/>', {'class': 'selectric-hide-select'});
+    var button = $('<b/>', {'class': 'button', 'text': '▾'});
+    var label = $('<span/>', {'class': 'label', 'text': firstLabelText});
+    var scroll = $('<div/>', {'class': 'selectric-scroll'});
+    var ul = $('<ul/>');
+    var listItems = [];
+
+    $options.map(function(ndx, option) {
+    	var option = $(option);
+    	var selected = option.prop('selected') ? 'selected' : '';
+    	var disabled = option.prop('disabled') ? ' disabled' : '';
+    	var value = option.attr('value');
+    	listItems.push($('<li/>', {
+    		'class': selected + disabled,
+    		'text': option.html(),
+    		'value': value
+    	}));
+    });
+
+    $this.hide();
+
+    $this.wrap(wrapper).wrap(hide);
+    var $wrapper = $this.parents('.selectric-wrapper');
+    $wrapper.append(selectric.append(label, button));
+    $wrapper.append(items.append(scroll.append(ul)));
+    $wrapper.find('ul').append(listItems);
+
+    var $selectric = $wrapper.find('.selectric');
+    var $items = $wrapper.find('.selectric-items');
+    var li = $items.find('li')[0];
+    var $li = $(li);
+    var $label = $wrapper.find('.label');
+
+    var itemHeight =
+      (parseInt($li.css('padding-top')) +
+      parseInt($li.css('padding-bottom')) +
+      parseInt($li.css('line-height'))) * parseInt(settings.maxSize);
+
+    var selectedItems = [];
+
+    $(document).on('click', function(e) {
+      var $el = $(e.target);
+      console.log($el)
+
+      if ($el.parents('.select-box_multiple').length) {
+
+        if ($el.is('.selectric') || $el.parent().is('.selectric')) {
+          if ($wrapper.is('.selectric-open')) {
+            $wrapper.removeClass('selectric-open');
+            $items.hide();
+          } else {
+            $wrapper.addClass('selectric-open');
+            $items.width($selectric.outerWidth()).height(itemHeight).show();
+          }
+        }
+
+        if ($el.is('li')) {
+          if ($el.is('.disabled') || $el.is('.off')) return false;
+
+          if ($el.is('.selected')) {
+            var findedIndex = selectedItems.findIndex(function(el, ndx) {
+              return el.index === $el.index() && el.value === $el.val();
+            });
+
+            selectedItems.splice(findedIndex, 1);
+            $el.removeClass('selected');
+            $this.find('option:nth-child(' + ($el.index() + 1) + ')').prop('selected', false);
+          } else {
+            selectedItems.push({
+              index: $el.index(),
+              value: $el.val(),
+              text: $el.html()
+            });
+            $el.addClass('selected');
+            $this.find('option:nth-child(' + ($el.index() + 1) + ')').prop('selected', true);
+          }
+        }
+
+        if ($el.is('.cancel') || $el.parent().is('.cancel')) {
+          selectedItems = [];
+          $options.each(function() {$(this).prop('selected', false);})
+          $items.find('li').removeClass('selected');
+        }
+
+        if (selectedItems.length) {
+          if (!$selectric.is('.active')) {
+            $selectric.addClass('active');
+            $selectric.append('<div class="cancel"><span></span></div>');
+          }
+          $label.html(selectedItems.map(function(el, ndx) {
+            return ndx === 0 ? el.text : ', ' + el.text;
+          }));
+
+          if (selectedItems.length === 3) {
+            $items.find('li:not(.selected)').each(function() {
+              $(this).addClass('off');
+            });
+          } else {
+            $items.find('li.off').each(function() {
+              $(this).removeClass('off');
+            });
+          }
+        } else {
+          $selectric.removeClass('active');
+          $selectric.find('.cancel').remove();
+          $label.html(firstLabelText);
+        }
+
+      } else {
+        $wrapper.removeClass('selectric-open');
+        $items.hide();
+      }
+    });
+
+  }
+})(jQuery);
